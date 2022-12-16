bd01db5e19a92ac9d8fa619e34a639d3561a2b67
Olekasndr Smolin <smool-@ukr.net>
2022-12-16 01:09:47 +0100

    remove commented line

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-// app.use('/logo_twins.svg', express.static('icons/logo_twins.svg'));
 
 app.listen(3000);
